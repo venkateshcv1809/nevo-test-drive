bbe68ee3a4dd4c0a5978e73552874ecd7860c8f9
VENKATESH C V <venkateshvairam@gmail.com>
2026-03-14 19:30:05 +0530

    infra: Setup configuration for web app
    - Web UI using Next.JS
    - Styles using Tailwind CSS
    - Default setup for Nx and Typescript

---
+++ apps/web/next.config.js
@@ -0,0 +1,27 @@
+//@ts-check
+
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+    // Use this to set Nx-specific options
+    // See: https://nx.dev/recipes/next/next-config-setup
+    nx: {},
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/api/:path*',
+            },
+        ];
+    },
+};
+
+const plugins = [
+    // Add more Next.js plugins to this list if needed.
+    withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
