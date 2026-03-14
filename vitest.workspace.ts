b39b049a597b8fa393757fc0ab77c7d0e98bae8f
VENKATESH C V <venkateshvairam@gmail.com>
2026-03-14 18:17:33 +0530

    infra: Add git pre-commit hook
    - Install lint-staged package
    - Add commands and config to process lint and format of staged files

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,16 +1,16 @@
 export default [
     '**/vite.config.{mjs,js,ts,mts}',
     '**/vitest.config.{mjs,js,ts,mts}',
-    
+
     '**/apps/*/vite.config.{mjs,js,ts,mts}',
     '**/libs/*/vite.config.{mjs,js,ts,mts}',
-    
+
     '**/*.test.{ts,tsx,js,jsx}',
     '**/*.spec.{ts,tsx,js,jsx}',
-    
+
     'vitest.config.ts',
     'vitest.workspace.ts',
-    
+
     '!**/dist/**',
     '!**/node_modules/**',
 ];
